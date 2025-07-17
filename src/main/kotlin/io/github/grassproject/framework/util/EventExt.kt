c93bc549ad5c5e89e775deeebc372d837b5e31cc
MrJimin <aa090402@naver.com>
2025-07-18 08:21:47 +0900

    add GUI but It not better

--- src/main/kotlin/io/github/grassproject/framework/util/EventExt.kt
+++ src/main/kotlin/io/github/grassproject/framework/util/EventExt.kt
@@ -3,7 +3,10 @@ package io.github.grassproject.framework.util
 import io.github.grassproject.framework.GPFrameworkPlugin
 import org.bukkit.Bukkit
 import org.bukkit.event.Event
+import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
 
 fun Listener.register() {
     GPFrameworkPlugin.instance.server.pluginManager.registerEvents(this, GPFrameworkPlugin.instance)
@@ -11,4 +14,24 @@ fun Listener.register() {
 
 fun Event.call() {
     Bukkit.getServer().pluginManager.callEvent(this)
+}
+
+inline fun <reified T : Event> event(
+    crossinline handler: T.() -> Unit
+): RegisteredEvent {
+    val plugin = JavaPlugin.getProvidingPlugin(object {}::class.java)
+    val listener = object : Listener {
+        @EventHandler
+        fun onEvent(event: T) {
+            handler(event)
+        }
+    }
+    Bukkit.getPluginManager().registerEvents(listener, plugin)
+    return RegisteredEvent(listener)
+}
+
+class RegisteredEvent(private val listener: Listener) {
+    fun unregister() {
+        HandlerList.unregisterAll(listener)
+    }
 }
