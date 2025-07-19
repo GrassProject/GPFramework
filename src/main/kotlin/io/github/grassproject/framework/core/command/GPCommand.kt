6d2277fef5d1c8ab86883a98aa672d860a81386c
Jimin Seo <aa090402@naver.com>
2025-07-19 14:55:48 +0900

    GPCommand

--- src/main/kotlin/io/github/grassproject/framework/core/command/GPCommand.kt
+++ src/main/kotlin/io/github/grassproject/framework/core/command/GPCommand.kt
@@ -1,8 +1,13 @@
 package io.github.grassproject.framework.core.command
 
 import io.github.grassproject.framework.core.GPPlugin
+import org.bukkit.command.Command
+import org.bukkit.permissions.PermissionDefault
 
 abstract class GPCommand<T : GPPlugin>(
     val plugin: T,
-) {
+    val names: List<String>,
+    permissionDefault: PermissionDefault = PermissionDefault.TRUE
+) : Command(names.first()) {
+    
 }
