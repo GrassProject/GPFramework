ced030f390815180f91f388f971921bab5df51f6
apo2073 <apo2073@outlook.com>
2025-08-28 15:00:51 +0900

    .

--- src/main/kotlin/io/github/grassproject/framework/core/events/GPEvent.kt
+++ src/main/kotlin/io/github/grassproject/framework/core/events/GPEvent.kt
@@ -1,12 +1,22 @@
 package io.github.grassproject.framework.core.events
 
+import io.github.grassproject.framework.core.GPPlugin
+import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
+import org.bukkit.event.EventHandler
 import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
 
 open class GPEvent(
     val name:String,
     isAsync:Boolean=false
-):Event(isAsync) {
+):Event(isAsync), Cancellable {
+    private var cancelled:Boolean=false;
+    override fun isCancelled(): Boolean = cancelled
+    override fun setCancelled(cancel: Boolean) {
+        this.cancelled=cancel
+    }
+
     override fun getEventName() = this.name
     override fun getHandlers(): HandlerList = getHandlerList()
     companion object {
@@ -22,7 +32,13 @@ open class GPPlayerEvent(
     player: org.bukkit.entity.Player,
     val name:String,
     isAsync:Boolean=false
-): org.bukkit.event.player.PlayerEvent(player, isAsync) {
+): org.bukkit.event.player.PlayerEvent(player, isAsync), Cancellable {
+    private var cancelled:Boolean=false;
+    override fun isCancelled(): Boolean = cancelled
+    override fun setCancelled(cancel: Boolean) {
+        this.cancelled=cancel
+    }
+
     override fun getEventName() = this.name
     override fun getHandlers(): HandlerList = getHandlerList()
     companion object {
